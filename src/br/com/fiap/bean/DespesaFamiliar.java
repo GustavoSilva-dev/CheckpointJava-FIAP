2e705c7b8633f5f5576d6c671722bda8585b8bc1
Gustavo Silva <gustavosilvaxx25@gmail.com>
2026-03-23 20:43:16 -0300

    Adicionando arquivos para o Checkpoint de Java

---
+++ src/br/com/fiap/bean/DespesaFamiliar.java
@@ -0,0 +1,22 @@
+package br.com.fiap.bean;
+
+// rm573880 - Gustavo Soares Silva
+public class DespesaFamiliar {
+    public double rendaFamiliar;
+    public double gastoComLuz;
+    public int numeroDeMoradores;
+    public double gastoComAgua;
+    public double gastoComInternet;
+    public double valorMensalidadeDaAcademia;
+
+    public double calcularTotalDeDespesas(){
+        double mensalidadeTotal = valorMensalidadeDaAcademia * numeroDeMoradores;
+        double totalDespesas = gastoComLuz + gastoComAgua + gastoComInternet + mensalidadeTotal;
+        return totalDespesas;
+    }
+
+    public double calcularRendaFamiliarLiquida() {
+        double rendaLiquida = rendaFamiliar - calcularTotalDeDespesas();
+        return rendaLiquida;
+    }
+}
---
+++ src/br/com/fiap/main/Main.java
@@ -0,0 +1,31 @@
+package br.com.fiap.main;
+import br.com.fiap.bean.*;
+import java.util.Scanner;
+
+// rm573880 - Gustavo Soares Silva
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        DespesaFamiliar desp = new DespesaFamiliar();
+
+        try {
+            System.out.println("Digite a sua renda familiar total:");
+            desp.rendaFamiliar = scan.nextDouble();
+            System.out.println("Digite o número de moradores da sua casa:");
+            desp.numeroDeMoradores = scan.nextInt();
+            System.out.println("Digite o gasto com conta de luz:");
+            desp.gastoComLuz = scan.nextDouble();
+            System.out.println("Digite o gasto com conta de água:");
+            desp.gastoComAgua = scan.nextDouble();
+            System.out.println("Digite o gasto com conta de internet:");
+            desp.gastoComInternet = scan.nextDouble();
+            System.out.println("Digite o gasto com a mensalidade da academia:");
+            desp.valorMensalidadeDaAcademia = scan.nextDouble();
+
+            System.out.printf("Renda Familiar Total: R$%.3f, Gasto Total com Despesas: R$%.3f, Renda Familiar Líquida: R$%.3f", desp.rendaFamiliar, desp.calcularTotalDeDespesas(), desp.calcularRendaFamiliarLiquida());
+        } catch (Exception e) {
+            System.out.println("--- Digito incorreto, tente novamente ;( ---");
+        }
+
+    }
+}
